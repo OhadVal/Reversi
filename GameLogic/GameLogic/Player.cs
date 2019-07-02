1006611a0da0d0078323fe43cff7896c4b5a2deb
OhadVal <42318947+OhadVal@users.noreply.github.com>
2019-07-02 12:53:18 +0300

    add Player class

---
+++ GameLogic/GameLogic/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class Player
+    {
+        private readonly string rName;
+        private string mNotGonnaStay;
+
+        public Player(string rName, string mNotGonnaStay)
+        {
+            this.rName = rName;
+            this.mNotGonnaStay = mNotGonnaStay;
+        }
+    }
+}
